b6d9abe991c582c7a4cfecbf476147c079326b8f
Juan Mangueira <juan.mangueira@obycapital.com.br>
2025-06-16 22:53:48 -0300

    Configuração: Corrige colunas da tabela de cenários e ajusta layout da sidebar

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 
 def main():
     if 'cenarios' not in st.session_state:
-        st.session_state['cenarios'] = pd.DataFrame(columns=['Probabilidade', 'Retorno'])
+        st.session_state['cenarios'] = pd.DataFrame(columns=['Ativo', 'Probabilidade', 'Retorno'])
     st.title("Calculadora Risco e Retorno")
     # with st.sidebar:
     st.subheader('Cenários')
@@ -19,34 +19,35 @@ def main():
         st.session_state['ativos'] = ['Ativo1', 'Ativo2', 'Ativo3']
         st.session_state['pesos'] = [0.5, 0.3, 0.2]
     with st.sidebar:
-        left, midle, right = st.columns([4, 1, 1], vertical_alignment="top")
+        left, midle, right = st.columns([3, 2, 1], vertical_alignment="top")
         left.subheader('Ativos')
 
 
-        if midle.button('',icon=':material/add:'):
+        if right.button('',icon=':material/add:'):
             st.session_state.ativos.append('')
             st.session_state.pesos.append(0.0)
             st.rerun()
 
-        if right.button('',icon=':material/remove:'):
-            if len(st.session_state.ativos)>1:
-                st.session_state.ativos.pop()
-                st.session_state.pesos.pop()
-                st.rerun()
-        left, right = st.columns(2)
         for i, ativo in enumerate(st.session_state.ativos):
+            left, midle, right = st.columns([3, 2, 1], vertical_alignment="bottom")
             with left:
                 st.session_state.ativos[i] = st.text_input(
                     f'Ativo {i+1}', 
                     st.session_state.ativos[i], 
                     key=f'a{i}'
                 )
-            with right:
+            with midle:
                 st.session_state.pesos[i] = st.number_input(
                     f'Peso {i+1}', 
                     st.session_state.pesos[i], 
                     key=f'p{i}'
                 )
+            with right:
+                st.write('')
+                if st.button(":material/remove:", key=f"del_{i}"):
+                    st.session_state.ativos.pop(i)
+                    st.session_state.pesos.pop(i)
+                    st.rerun()
 
     st.data_editor(st.session_state['cenarios'], num_rows= "dynamic")
     st.write(st.session_state)
